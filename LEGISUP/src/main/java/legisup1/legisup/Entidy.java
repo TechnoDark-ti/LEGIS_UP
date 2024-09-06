3b55927e927e532c98e19bf3caabf9e600ed1740
Márcio Moda <69868170+TechnoDark-ti@users.noreply.github.com>
2024-09-06 19:15:34 -0300

    Refatorando o projeto
    Atualizando o codigo fonte do projeto para o NetBeans

---
+++ LEGISUP/src/main/java/legisup1/legisup/Entidy.java
@@ -0,0 +1,24 @@
+package legisup1.legisup;
+
+public class Entidy {
+    private String nome;
+    private String data;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+    
+    
+}
