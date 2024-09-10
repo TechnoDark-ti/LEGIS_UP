9f78c37cb2e0fefb3497a2a689c093b5b995be9c
Márcio Moda <69868170+TechnoDark-ti@users.noreply.github.com>
2024-09-10 20:12:47 -0300

    Create Departamento.java

---
+++ LEGISUP/src/main/java/Classes/Departamento.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes;
+
+/**
+ *
+ * @author marcio.moda
+ */
+public class Departamento {
+    public int departamento_id;
+    public String nome;
+    public String Descricao;
+
+    public Departamento(int departamento_id, String nome, String Descricao) {
+        this.departamento_id = departamento_id;
+        this.nome = nome;
+        this.Descricao = Descricao;
+    }
+
+    public int getDepartamento_id() {
+        return departamento_id;
+    }
+
+    public void setDepartamento_id(int departamento_id) {
+        this.departamento_id = departamento_id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return Descricao;
+    }
+
+    public void setDescricao(String Descricao) {
+        this.Descricao = Descricao;
+    }
+    
+}
